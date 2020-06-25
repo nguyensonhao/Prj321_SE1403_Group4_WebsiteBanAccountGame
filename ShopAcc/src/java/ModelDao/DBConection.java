15b6dc988c1b4daa17958826b11ab7244e9603b7
quanghien140311 <63026166+quanghien140311@users.noreply.github.com>
2020-06-25 20:56:14 +0700

    sql
    Hien

--- ShopAcc/src/java/ModelDao/DBConection.java
+++ ShopAcc/src/java/ModelDao/DBConection.java
@@ -5,10 +5,33 @@
  */
 package ModelDao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Hao Nguyen
  */
 public class DBConection {
-    
+     Connection conn;
+
+    public DBConection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shopacc", "root", "");
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    Connection getConnection() {
+        return conn ;
+    }
 }
