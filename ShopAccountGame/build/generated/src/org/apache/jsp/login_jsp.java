218f1b096968bfb857451546e410e0012b240dee
Nguyen Son Hao <56017043+nguyensonhao@users.noreply.github.com>
2020-07-21 23:29:26 +0700

    Delete lỗi

---
+++ ShopAccountGame/build/generated/src/org/apache/jsp/login_jsp.java
@@ -0,0 +1,164 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html lang=\"en\">\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <title>Đăng Nhập</title>\r\n");
+      out.write("        <meta charset=\"UTF-8\">\r\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n");
+      out.write("        <!--===============================================================================================-->\t\r\n");
+      out.write("        <link rel=\"icon\" type=\"image/png\" href=\"Asset/images/icons/favicon.ico\"/>\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/vendor/bootstrap/css/bootstrap.min.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/fonts/font-awesome-4.7.0/css/font-awesome.min.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/fonts/iconic/css/material-design-iconic-font.min.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/vendor/animate/animate.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\t\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/vendor/css-hamburgers/hamburgers.min.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/vendor/animsition/css/animsition.min.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/vendor/select2/select2.min.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\t\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/vendor/daterangepicker/daterangepicker.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/css/util.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"Asset/css/main.css\">\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("    </head>\r\n");
+      out.write("    <body>\r\n");
+      out.write("\r\n");
+      out.write("        <div class=\"limiter\">\r\n");
+      out.write("            <div class=\"container-login100\" style=\"background-image: url('images/bg-01.jpg');\">\r\n");
+      out.write("                <div class=\"wrap-login100\">\r\n");
+      out.write("                    <div class=\"login100-pic js-tilt\" data-tilt>\r\n");
+      out.write("                        <img src=\"Asset/images/img-01.png\" alt=\"IMG\">\r\n");
+      out.write("                    </div>\r\n");
+      out.write("\r\n");
+      out.write("                    <form class=\"login100-form validate-form\" method=\"post\" action=\"/LoginController\">\r\n");
+      out.write("                        <input type=\"hidden\" name=\"page\" value=\"login-form\">\r\n");
+      out.write("                        \r\n");
+      out.write("                        <span class=\"login100-form-logo\">\r\n");
+      out.write("                            <i class=\"zmdi zmdi-landscape\"></i>\r\n");
+      out.write("                        </span>\r\n");
+      out.write("\r\n");
+      out.write("                        <span class=\"login100-form-title p-b-34 p-t-27\">\r\n");
+      out.write("                            Đăng Nhập\r\n");
+      out.write("                        </span>\r\n");
+      out.write("\r\n");
+      out.write("                        <div class=\"wrap-input100 validate-input\" data-validate = \"Enter username\">\r\n");
+      out.write("                            <input class=\"input100\" type=\"text\" name=\"txtUser\" placeholder=\"Tài Khoản\">\r\n");
+      out.write("                            <span class=\"focus-input100\" data-placeholder=\"&#xf207;\"></span>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("\r\n");
+      out.write("                        <div class=\"wrap-input100 validate-input\" data-validate=\"Password is required\">\r\n");
+      out.write("                            <input class=\"input100\" type=\"password\" name=\"txtPassword\" placeholder=\"Mật Khẩu\">\r\n");
+      out.write("                            <span class=\"focus-input100\" data-placeholder=\"&#xf191;\"></span>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("\r\n");
+      out.write("                        <div class=\"contact100-form-checkbox\">\r\n");
+      out.write("                            <input class=\"input-checkbox100\" id=\"ckb1\" type=\"checkbox\" name=\"remember-me\">\r\n");
+      out.write("                            <label class=\"label-checkbox100\" for=\"ckb1\">\r\n");
+      out.write("                                Nhớ Tài Khoản\r\n");
+      out.write("                            </label>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("                        <div align=\"center\"><font color='red'><label id=\"send-error\" class=\"error\" for=\"checksend\" style=\"display: block\"></label></font></div><br>\r\n");
+      out.write("                        <div class=\"container-login100-form-btn\">\r\n");
+      out.write("                            <button class=\"login100-form-btn\">\r\n");
+      out.write("                                Đăng Nhập\r\n");
+      out.write("                            </button>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("\r\n");
+      out.write("                        <div class=\"text-center p-t-90\">\r\n");
+      out.write("                            <a class=\"txt1\" href=\"REGISTRATION.jsp\">\r\n");
+      out.write("                                REGISTRATION\r\n");
+      out.write("                            </a>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("                    </form>\r\n");
+      out.write("                </div>\r\n");
+      out.write("            </div>\r\n");
+      out.write("        </div>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        <div id=\"dropDownSelect1\"></div>\r\n");
+      out.write("\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <script src=\"Asset/vendor/jquery/jquery-3.2.1.min.js\"></script>\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <script src=\"Asset/vendor/animsition/js/animsition.min.js\"></script>\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <script src=\"Asset/vendor/bootstrap/js/popper.js\"></script>\r\n");
+      out.write("        <script src=\"Asset/vendor/bootstrap/js/bootstrap.min.js\"></script>\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <script src=\"Asset/vendor/select2/select2.min.js\"></script>\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <script src=\"Asset/vendor/daterangepicker/moment.min.js\"></script>\r\n");
+      out.write("        <script src=\"Asset/vendor/daterangepicker/daterangepicker.js\"></script>\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <script src=\"Asset/vendor/countdowntime/countdowntime.js\"></script>\r\n");
+      out.write("        <!--===============================================================================================-->\r\n");
+      out.write("        <script src=\"Asset/js/main.js\"></script>\r\n");
+      out.write("\r\n");
+      out.write("    </body>\r\n");
+      out.write("</html>\r\n");
+      out.write("\r\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
